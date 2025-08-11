16694e309c511a46f9544939b631d6fbdb34b6c4
Nils Haberkamp <haberkamp.n@gmail.com>
2025-08-11 17:43:00 +0200

    Release 0.0.2

--- src/parser.c
+++ src/parser.c
@@ -1975,8 +1975,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_shopware_twig(void) {
     .name = "shopware_twig",
     .max_reserved_word_set_size = 0,
     .metadata = {
-      .major_version = 0,
-      .minor_version = 1,
+      .major_version = 1,
+      .minor_version = 0,
       .patch_version = 0,
     },
   };
